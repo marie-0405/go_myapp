da1f5d7d07f234012118cc4850bbae983ed42e11
marie-0405 <sirisuamari0405@yahoo.co.jp>
2022-12-14 00:07:14 +0900

    Working with layouts

--- pkg/handler/handlers.go
+++ pkg/handler/handlers.go
@@ -8,10 +8,10 @@ import (
 
 // Home is the home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "home.page.html")
+	render.RenderTemplate(w, "home.page.tmpl")
 }
 
 // About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "about.page.html")
+	render.RenderTemplate(w, "about.page.tmpl")
 }
